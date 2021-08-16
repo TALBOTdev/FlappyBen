99e7fef425417c8408db266ed38d3dd56fcaaec8
Ben Talbot <btalbot00@gmail.com>
2021-08-16 17:20:24 -0600

    Final Project Commit

---
+++ Assets/Scripts/TapController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour
+{
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+    public static event PlayerDelegate OnPlayerScored;
+
+    public float tapForce = 10;
+    public float tiltSmooth = 5;
+    public Vector3 startPos;
+
+    public AudioSource tapAudio;
+    public AudioSource scoreAudio;
+    public AudioSource dieAudio;
+
+    Rigidbody2D rigidbody;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
+    GameManager game;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -45);
+        forwardRotation = Quaternion.Euler(0, 0, 45);
+        game = GameManager.Instance;
+        rigidbody.simulated = false;
+        
+    }
+
+    void OnEnable()
+    {
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
+
+    void OnDisable()
+    {
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+
+    void OnGameStarted()
+    {
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.simulated = true;
+    }
+
+    void OnGameOverConfirmed()
+    {
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity;
+    }
+
+    void Update()
+    {
+        if (game.GameOver) return;
+        if (Input.GetMouseButtonDown(0))
+        {
+            tapAudio.Play();
+            transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Scorezone")
+        {
+            // Register a score event
+            OnPlayerScored(); // Event sent to GameManager;
+
+            // Play a sound
+            scoreAudio.Play();
+        }
+
+        if (collision.gameObject.tag == "Deadzone")
+        {
+            rigidbody.simulated = false;
+            //Register a dead event
+            OnPlayerDied(); // Event sent to GameManager
+
+            // Play a sound
+            dieAudio.Play();
+        }
+    }
+}
